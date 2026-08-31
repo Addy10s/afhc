4ee77e8cca88e6db32ac8b1fd05d5ebada6a3aa9
Adrian Tennies <adrian@tennies.us>
2026-08-30 23:51:26 -0500

    i hate containerization, and i hate hugo, svelte my BELOVED

---
+++ src/routes/topbar.svelte
@@ -0,0 +1,12 @@
+<script>
+	import favicon from '$lib/assets/afhc.svg';
+</script>
+
+
+<div class="flex">
+    <img alt="The project logo" src={favicon} width=128 />
+    <div>
+        <h1> Alternative for Hack Club </h1>
+        <h2> The Main Opposition Party of Parliament</h2>
+    </div>
+</div>
---
+++ tailwind.config.ts
@@ -0,0 +1,9 @@
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  corePlugins: {
+    preflight: false, // This disables the base CSS reset
+  }
+}
